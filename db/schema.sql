afd5b520116d627eed31cfbc5588e410a05752d0
Steve Fokas <sf487552@gmail.com>
2021-08-26 14:04:22 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE 
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
+);
